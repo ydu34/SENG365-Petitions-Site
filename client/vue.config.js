7378b030c46e50fa7dd032fd0cd72f9836d57df4
Yu Duan <ydu34@uclive.ac.nz>
2021-07-02 16:48:41 +1200

    Attempting to set publicPath in vue.config.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/SENG365-Petitions-Site/'
-        : '/'
+    publicPath: '/SENG365-Petitions-Site/'
 }
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/client/dist/',
+    publicPath: '/dist/',
     filename: 'build.js'
   },
   module: {
